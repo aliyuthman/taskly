7c4ad3d269f0b983cd487040f465fc92328e4360
Aliyu Usman <aliyuusman64@gmail.com>
2024-08-20 10:53:04 +0100

    request push notification permission

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,10 +1,22 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 
 export default function CounterScreen() {
+  const handleRequestPermission = async () => {
+    const result = await registerForPushNotificationsAsync();
+    console.log(result);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.buttonText}>Go to /Idea</Text>
-
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.8}
+        onPress={handleRequestPermission}
+      >
+        <Text style={styles.buttonText}>Request permission</Text>
+      </TouchableOpacity>
       <Text style={styles.text}>Counter</Text>
     </View>
   );
@@ -21,8 +33,18 @@ const styles = StyleSheet.create({
     fontSize: 24,
   },
 
+  button: {
+    alignSelf: "center",
+    marginBottom: 18,
+    backgroundColor: theme.colorOrange,
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 12,
+  },
   buttonText: {
-    fontSize: 24,
-    color: "white",
+    color: theme.colorWhite,
+    letterSpacing: 1,
+    textTransform: "uppercase",
+    fontWeight: "bold",
   },
 });
